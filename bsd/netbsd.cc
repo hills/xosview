5d5b05aaa1ba6397bed1b2b91b4443b3a3dc70d6
bgrayson <bgrayson>
1996-11-19 06:28:26 +0000

    Added setting of kernel name, disk support, some sanity checking of kvm stuff

--- bsd/netbsd.cc
+++ bsd/netbsd.cc
@@ -9,22 +9,25 @@
 //
 // $Id$
 //
+#include "general.h"
 #include <stdio.h>
 #include <fcntl.h>
 #include <kvm.h>
 #include <nlist.h>
+#include <limits.h>		/*  For _POSIX2_LINE_MAX  */
 
 #include <err.h>                /*  For err(), warn(), etc.  BCG  */
 #include <sys/dkstat.h>         /*  For CPUSTATES, which tells us how
                                       many cpu states there are.  */
+#include <sys/disk.h>		/*  For disk statistics.  */
 
 #include <sys/socket.h>         /*  These two are needed for the  */
 #include <net/if.h>             /*    NetMeter helper functions.  */
 
 
 // ------------------------  local variables  ---------------------
-static kvm_t* kd = NULL;  //  This single kvm_t is shared by all
-                          //  of the kvm routines.
+kvm_t* kd = NULL;	//  This single kvm_t is shared by all
+                        //  of the kvm routines.
 
 //  This struct has the list of all the symbols we want from the kernel.
 static struct nlist nlst[] =
@@ -33,10 +36,12 @@ static struct nlist nlst[] =
 #define CP_TIME 0
 { "_ifnet" },
 #define IFNET 1
+{ "_disklist" },
+#define DISKLIST_SYM_INDEX	2
   {NULL}
   };
 
-static int initialized = 0;
+static char kernelFileName[_POSIX2_LINE_MAX];
 
 // ------------------------  utility functions  -------------------
 //  The following is an error-checking form of kvm_read.  In addition
@@ -46,6 +51,11 @@ static int initialized = 0;
 static inline void
 safe_kvm_read (u_long kernel_addr, void* user_addr, size_t nbytes)
 {
+    /*  Check for obvious bad symbols (i.e., from /netbsd when we
+     *  booted off of /netbsd.old), such as symbols that reference
+     *  0x00000000 (or anywhere in the first 256 bytes of memory).  */
+  if ((kernel_addr&0xffffff00) == 0)
+    errx(-1, "safe_kvm_read() was attempted on EA %#lx\n", kernel_addr);
   if (kvm_read (kd, kernel_addr, user_addr, nbytes)==-1)
     err(-1, "kvm_read() of kernel address %#lx", kernel_addr);
 }
@@ -59,16 +69,58 @@ safe_kvm_read_symbol (int nlstOffset, void* user_addr, size_t nbytes)
 }
 
 
+int
+ValidSymbol (int index)
+{
+  return ((nlst[index].n_value & 0xffffff00) != 0);
+}
+
+
+void
+NetBSDInit()
+{
+  kernelFileName[0] = '\0';
+}
+
+void
+SetKernelName(const char* const kernelName)
+{
+  if (strlen(kernelName)>=_POSIX2_LINE_MAX)
+  {
+    fprintf (stderr, "Error:  kernel file name of '%s' is too long!\n",
+      kernelName);
+    exit(1);
+  }
+  strcpy(kernelFileName, kernelName);
+}
+
 void
 OpenKDIfNeeded()
 {
-  char unusederrorstring[256];
+  char unusederrorstring[_POSIX2_LINE_MAX];
 
   if (kd) return; //  kd is non-NULL, so it has been initialized.  BCG
 
-  if ((kd = kvm_openfiles (NULL, NULL, NULL, O_RDONLY, unusederrorstring))
+    /*  Open it read-only, for a little added safety.  */
+    /*  If the first character of kernelFileName is not '\0', then use
+	that kernel file.  Otherwise, use the default kernel, by
+	specifying NULL.  */
+  if ((kd = kvm_openfiles ((kernelFileName[0]) ? kernelFileName : NULL,
+			    NULL, NULL, O_RDONLY, unusederrorstring))
       == NULL)
 	  err (-1, "OpenKDIfNeeded():kvm-open()");
+    /*  Now grab the symbol offsets for the symbols that we want.  */
+  kvm_nlist (kd, nlst);
+
+  //  Look at all of the returned symbols, and check for bad lookups.
+  //  (This may be unnecessary, but better to check than not to...  )
+  struct nlist * nlp = nlst;
+  while (nlp && nlp->n_name) {
+    if ((nlp->n_type == 0) || (nlp->n_value == 0))
+      /*errx (-1, "kvm_nlist() lookup failed for symbol '%s'.", nlp->n_name);*/
+      warnx ("kvm_nlist() lookup failed for symbol '%s'.", nlp->n_name);
+    nlp++;
+  }
 }
 
 
@@ -95,26 +147,11 @@ void
 NetBSDNetInit()
 {
   OpenKDIfNeeded();
-  kvm_nlist (kd, nlst);
-
-  //  Look at all of the returned symbols, and check for bad lookups.
-  //  (This may be unnecessary, but better to check than not to...  )
-  struct nlist * nlp = nlst;
-  while (nlp && nlp->n_name) {
-    if ((nlp->n_type == 0) || (nlp->n_value == 0))
-      errx (-1, "kvm_nlist() lookup failed for symbol '%s'.", nlp->n_name);
-    nlp++;
-  }
-  initialized = 1;
 }
 
 void
 NetBSDGetNetInOut (long long * inbytes, long long * outbytes)
 {
-  if (!initialized)
-    NetBSDNetInit();
-
-
   struct ifnet * ifnetp;
   struct ifnet ifnet;
   //char ifname[256];
@@ -144,3 +181,37 @@ NetBSDGetNetInOut (long long * inbytes, long long * outbytes)
 }
 
 
+/*  ---------------------- Disk Meter stuff  -----------------  */
+int
+NetBSDDiskInit() {
+  OpenKDIfNeeded(); 
+  return ValidSymbol(DISKLIST_SYM_INDEX);
+}
+
+void
+NetBSDGetDiskXFerBytes (unsigned long long *bytesXferred)
+{
+  /*  This function is a little tricky -- we have to iterate over a
+   *  list in kernel land.  To make things simpler, data structures
+   *  and pointers for objects in kernel-land have kvm tacked on front
+   *  of their names.  Thus, kvmdiskptr points to a disk struct in
+   *  kernel memory.  kvmcurrdisk is a copy of the kernel's struct,
+   *  and it has pointers in it to other structs, so it also is
+   *  prefixed with kvm.  */
+  struct disklist_head kvmdisklist;
+  struct disk *kvmdiskptr;
+  struct disk kvmcurrdisk;
+  safe_kvm_read_symbol (DISKLIST_SYM_INDEX, &kvmdisklist, sizeof(kvmdisklist));
+  kvmdiskptr = kvmdisklist.tqh_first;
+  while (kvmdiskptr != NULL)
+  {
+    safe_kvm_read ((unsigned int)kvmdiskptr, &kvmcurrdisk, sizeof(kvmcurrdisk));
+      /*  Add up the contribution from this disk.  */
+    *bytesXferred += kvmcurrdisk.dk_bytes;
+#ifdef DEBUG
+    printf ("Got %#x (lower 32bits)\n", (int) (*bytesXferred & 0xffffffff));
+#endif
+    kvmdiskptr = kvmcurrdisk.dk_link.tqe_next;
+  }
+}
+  
