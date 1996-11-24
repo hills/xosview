2f652e56464234ee915c937703d82de576a92426
bgrayson <bgrayson>
1996-11-24 04:34:12 +0000

    Added CVSID stuff, added DiskMeter, changed all strcmp's to use strcasecmp() (case insensitive)

--- bsd/MeterMaker.cc
+++ bsd/MeterMaker.cc
@@ -8,45 +8,46 @@
 //
 // $Id$
 //
-
+#include <stdlib.h>
+#include "general.h"
 #include "MeterMaker.h"
 #include "xosview.h"
-
 #include "cpumeter.h"
 #include "memmeter.h"
 #include "swapmeter.h"
 #include "netmeter.h"
 #include "loadmeter.h"
+#include "diskmeter.h"
 //#include "intmeter.h"  //  These two are not yet supported under NetBSD.
 //#include "serialmeter.h"
 
-
-#include <stdlib.h>
+CVSID_DOT_H2(PLLIST_H_CVSID);
+CVSID_DOT_H(METERMAKER_H_CVSID);
+CVSID("$Id$");
 
 MeterMaker::MeterMaker(XOSView *xos){
   _xos = xos;
-
 }
 
 void MeterMaker::makeMeters(void){
   //  check for the loadmeter
-  if (!strcmp(_xos->getResource("load"), "True"))
+  if (!strcasecmp(_xos->getResource("load"), "True"))
     push(new LoadMeter(_xos));
 
   // Standard meters (usually added, but users could turn them off)
-  if (!strcmp(_xos->getResource("cpu"), "True"))
+  if (!strcasecmp(_xos->getResource("cpu"), "True"))
     push(new CPUMeter(_xos));
-  if (!strcmp(_xos->getResource("mem"), "True"))
+  if (!strcasecmp(_xos->getResource("mem"), "True"))
     push(new MemMeter(_xos));
-  if (!strcmp(_xos->getResource("swap"), "True"))
+  if (!strcasecmp(_xos->getResource("swap"), "True"))
     push(new SwapMeter(_xos));
 
   // check for the net meter
-  //  FIXME  This check should be changed to check for the "net" resource.  BCG
-  if (strcmp(_xos->getResource("network"), "0"))
-  {
-    push(new NetMeter(_xos, atof(_xos->getResource("network"))));
-  }
+  if (!strcasecmp(_xos->getResource("net"), "True"))
+    push(new NetMeter(_xos, atof(_xos->getResource("networkBandwidth"))));
+
+  if (!strcasecmp(_xos->getResource("disk"), "True"))
+    push(new DiskMeter (_xos));
 
   //  The serial meters and the interrupt meter are not yet
   //  available for NetBSD.  BCG
