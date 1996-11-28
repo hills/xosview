536684cf06502350f9b18baf1c5e916dedddc593
bgrayson <bgrayson>
1996-11-28 22:15:41 +0000

    Initial checkin

---
+++ version.cc
@@ -0,0 +1,4 @@
+  /*  This file is included by xosview.cc, to set up the version
+   *  string.  */
+static const char * const versionString = "xosview version 1.4.2";
+static const char * const version_cc_cvsID = "$Id$";
