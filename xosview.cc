fbf05ed1e2757bcbfd7734f85b96d2d9812a82c0
bgrayson <bgrayson>
1997-01-14 18:28:11 +0000

    Now use MAX_SAMPLES_PER_SECOND for usleep calculations

--- xosview.cc
+++ xosview.cc
@@ -245,11 +245,11 @@ void XOSView::run( void ){
 
       flush();
     }
-    unsigned long sleeptime = 1000*SAMPLE_MSECS;
+    unsigned long usleeptime = 1000000/MAX_SAMPLES_PER_SECOND;
 #ifdef HAVE_USLEEP
-    usleep( sleeptime );
+    usleep( usleeptime );
 #else
-    usleep_via_select ( sleeptime );
+    usleep_via_select ( usleeptime );
 #endif
     counter = (counter + 1) % 5;    
   }
