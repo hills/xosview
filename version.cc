e16d91c82325d51a380a9c728c4928a4a01761d5
bgrayson <bgrayson>
1997-02-14 06:18:39 +0000

    Bumped version up to 1.4.3alpha

--- version.cc
+++ version.cc
@@ -1,4 +1,4 @@
   /*  This file is included by xosview.cc, to set up the version
    *  string.  */
-static const char * const versionString = "xosview version 1.4.2";
+static const char * const versionString = "xosview version 1.4.3alpha";
 static const char * const version_cc_cvsID = "$Id$";
