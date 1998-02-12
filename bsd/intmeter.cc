f5da9e641ebc2a5682fea85d330d8fe42ddd3109
bgrayson <bgrayson>
1998-02-12 04:23:51 +0000

    Initial checkin

---
+++ bsd/intmeter.cc
@@ -0,0 +1,55 @@
+//  
+//  Copyright (c) 1994, 1995 by Mike Romberg ( romberg@fsl.noaa.gov )
+//
+//  This file may be distributed under terms of the GPL
+//
+//
+// $Id$
+//
+#include "intmeter.h"
+#include "xosview.h"
+#include <fstream.h>
+#include <stdlib.h>
+#include "kernel.h"
+
+
+
+
+IntMeter::IntMeter( XOSView *parent,
+                    const char *, const char *, int dolegends,
+                    int dousedlegends )
+  //  For platforms like the Mac with less than 16 interrupts, we
+  //  ought to auto-detect how many there are.  I'll delay adding the
+  //  code to do this for a few weeks, so that we can get version 1.6
+  //  out the door.
+  : BitMeter( parent, "INTS", "IRQs (0 - 15)", 16, 
+              dolegends, dousedlegends ) {
+  for ( int i = 0 ; i < 16 ; i++ )
+    irqs_[i] = lastirqs_[i] = 0;
+}
+
+IntMeter::~IntMeter( void ){
+}
+
+void IntMeter::checkevent( void ){
+  getirqs();
+
+  for ( int i = 0 ; i < 16 ; i++ ){
+    bits_[i] = ((irqs_[i] - lastirqs_[i]) != 0);
+    lastirqs_[i] = irqs_[i];
+  }
+
+  BitMeter::checkevent();
+}
+
+void IntMeter::checkResources( void ){
+  BitMeter::checkResources();
+  onColor_  = parent_->allocColor( parent_->getResource( "intOnColor" ) );
+  offColor_ = parent_->allocColor( parent_->getResource( "intOffColor" ) );
+}
+
+void 
+IntMeter::getirqs( void )
+{
+    NetBSDGetIntrStats (irqs_);
+}
---
+++ bsd/intmeter.h
@@ -0,0 +1,31 @@
+//  
+//  Copyright (c) 1994, 1995 by Mike Romberg ( romberg@fsl.noaa.gov )
+//
+//  This file may be distributed under terms of the GPL
+//
+//
+// $Id$
+//
+#ifndef _INTMETER_H_
+#define _INTMETER_H_
+
+#include "bitmeter.h"
+#include "kernel.h"
+
+class IntMeter : public BitMeter {
+public:
+  IntMeter( XOSView *parent,
+	    const char *title = "", const char *legend ="",
+	    int dolegends = 0, int dousedlegends = 0 );
+  ~IntMeter( void );
+
+  void checkevent( void );
+
+  void checkResources( void );
+protected:
+  unsigned long irqs_[NUM_INTR], lastirqs_[NUM_INTR];
+
+  void getirqs( void );
+};
+
+#endif
