5b56f85dab18dd8e79acb53376410e9c2f7aca0a
bgrayson <bgrayson>
1998-09-24 19:23:30 +0000

    Bumped version to 1.6.2.a

--- version.cc
+++ version.cc
@@ -1,4 +1,7 @@
   /*  This file is included by xosview.cc, to set up the version
    *  string.  */
-static const char * const versionString = "xosview version 1.6.1";
+/*  Version 1.6.2.a:  a snapshot that contains, among other
+ *  things, the merged-in patches from NetBSD pkgsrc, in addition
+ *  to fixes to allow NetBSD-alpha to mostly work.  bgrayson  */
+static const char * const versionString = "xosview version 1.6.2.a";
 static const char * const version_cc_cvsID = "$Id$";
