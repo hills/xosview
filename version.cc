3b4ddda89ef17e62b0ce15ab36ad34a4535725dc
bgrayson <bgrayson>
1999-01-18 09:52:12 +0000

    Bumped to version 1.6.2.b, for FreeBSD devstat support

--- version.cc
+++ version.cc
@@ -3,5 +3,7 @@
 /*  Version 1.6.2.a:  a snapshot that contains, among other
  *  things, the merged-in patches from NetBSD pkgsrc, in addition
  *  to fixes to allow NetBSD-alpha to mostly work.  bgrayson  */
-static const char * const versionString = "xosview version 1.6.2.a";
+/*  Version 1.6.2.b:  now has FreeBSD libdevstat support, for fixed
+    diskmeter.  bgrayson  */
+static const char * const versionString = "xosview version 1.6.2.b";
 static const char * const version_cc_cvsID = "$Id$";
