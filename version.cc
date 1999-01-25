940f8c5dc7421ed6a242825171f4c5ff93bb18a5
bgrayson <bgrayson>
1999-01-25 20:23:13 +0000

    Bumped version to 1.7.0

--- version.cc
+++ version.cc
@@ -5,5 +5,8 @@
  *  to fixes to allow NetBSD-alpha to mostly work.  bgrayson  */
 /*  Version 1.6.2.b:  now has FreeBSD libdevstat support, for fixed
     diskmeter.  bgrayson  */
-static const char * const versionString = "xosview version 1.6.2.b";
+/*  Version 1.7.0:  has pixmap support, OpenBSD interrupt meter,
+ *  NetBSD battery meter, some more Solaris support,
+ *  sliding-graphs, and more.  */
+static const char * const versionString = "xosview version 1.7.0";
 static const char * const version_cc_cvsID = "$Id$";
