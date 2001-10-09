7e23a5b1875da6523d3f512bd8450d0abef8367f
bgrayson <bgrayson>
2001-10-09 02:38:09 +0000

    Recommit of change lost during CVS repo move:  added check that the battery is present

--- bsd/btrymeter.cc
+++ bsd/btrymeter.cc
@@ -9,6 +9,7 @@
 //
 // $Id$
 //
+//  Only supported for NetBSD so far.
 #include "btrymeter.h"
 #include "xosview.h"
 #include <fstream.h>
@@ -48,6 +49,7 @@ void BtryMeter::checkevent( void ){
 
 void BtryMeter::getpwrinfo( void ){
   struct apm_power_info buff;
+  float minutes_left, max_left;
   int error = 0;
   int loadinfo = open(APMFILENAME, O_RDONLY, 0);
 
@@ -60,30 +62,37 @@ void BtryMeter::getpwrinfo( void ){
   error = ioctl(loadinfo, APM_IOC_GETPOWER, &buff);
 
   if (error != -1) {
+    if (buff.battery_state != APM_BATT_ABSENT) {
 
-    fields_[0] = (float)buff.battery_life;	// percent left
-    fields_[1] = 100.0 - fields_[0];		// percent used
-    float minutes_left = (float)buff.minutes_left; 
+      fields_[0] = (float)buff.battery_life;	// percent left
+      fields_[1] = 100.0 - fields_[0];		// percent used
+      minutes_left = (float)buff.minutes_left; 
 
-    //    minutes_left = (percent_left/100.0) * max_minutes;
-    //    minutes_left / (percent_left/100.0) = max_minutes;
-    //    minutes_left * 100.0 / percent_left = max_minutes;
-    float max_left = minutes_left * 100.0 / fields_[0];
+      //    minutes_left = (percent_left/100.0) * max_minutes;
+      //    minutes_left / (percent_left/100.0) = max_minutes;
+      //    minutes_left * 100.0 / percent_left = max_minutes;
+      if (minutes_left == 0) {
+        // This fix a bogus warning when the apm bios doesn't know
+        // the correct remaining time.
+        minutes_left = (float)0.01;
+      }
+      max_left = minutes_left * 100.0 / fields_[0];
   
-    //
-    // Set total_ so that the graphing methods know to set the
-    // relative sizes of the percent left and percent used graphs.
-    //
-    total_ = 100.0;
+      //
+      // Set total_ so that the graphing methods know to set the
+      // relative sizes of the percent left and percent used graphs.
+      //
+      total_ = 100.0;
 
-    //
-    // This sets the actual number of minutes left.  If the resource
-    // xosview*batteryUsedFormat is "float", then xosview will display
-    // that number to the left of the battery graph.  If it's
-    // "percent", then you get the percent remaining next to the
-    // graph.
-    //
-    setUsed(minutes_left, max_left);
+      //
+      // This sets the actual number of minutes left.  If the resource
+      // xosview*batteryUsedFormat is "float", then xosview will display
+      // that number to the left of the battery graph.  If it's
+      // "percent", then you get the percent remaining next to the
+      // graph.
+      //
+      setUsed(minutes_left, max_left);
+    }
   }
 
   close(loadinfo);
