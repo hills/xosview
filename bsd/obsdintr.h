ceef809c51b88344b3f7c245cc6c37a9be915a3d
bgrayson <bgrayson>
2002-07-14 03:45:13 +0000

    Oleg Safiullin's patch:  support both EGCS and i386.

--- bsd/obsdintr.h
+++ bsd/obsdintr.h
@@ -1,7 +1,17 @@
 #ifndef _OPENBSDINTR_H_
 #define _OPENBSDINTR_H_
 
-#ifdef i386
+/* XXX: gcc defines i386, but egcs defines __i386__ */
+#if defined(i386) && !defined(__i386__)
+#define __i386__
+#endif
+
+/* XXX: gcc defines pc532, but egcs defines __pc532__ */
+#if defined(pc532) && !defined(__pc532__)
+#define __pc532__
+#endif
+
+#ifdef __i386__
 struct intrhand {
 	int	(*ih_fun) __P((void *));
 	void	*ih_arg;
@@ -13,7 +23,7 @@ struct intrhand {
 };
 #endif
 
-#ifdef pc532
+#ifdef __pc532__
 struct iv {
 	void (*iv_vec)();
 	void *iv_arg;
