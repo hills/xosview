8c1cd17d21091411001a5325a23aebf24a740acc
romberg <romberg>
2004-06-01 03:07:40 +0000

    Fix the pagemeter to work with 2.6 kernels (linux)

--- linux/pagemeter.cc
+++ linux/pagemeter.cc
@@ -10,20 +10,30 @@
 #include "xosview.h"
 #include <fstream>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-
-static const char STATFILENAME[] = "/proc/stat";
 #define MAX_PROCSTAT_LENGTH 2048
 
 
 PageMeter::PageMeter( XOSView *parent, float max )
-  : FieldMeterGraph( parent, 3, "PAGE", "IN/OUT/IDLE" ){
+  : FieldMeterGraph( parent, 3, "PAGE", "IN/OUT/IDLE" ),
+  _vmstat(false), _statFileName("/proc/stat"){
   for ( int i = 0 ; i < 2 ; i++ )
     for ( int j = 0 ; j < 2 ; j++ )
       pageinfo_[j][i] = 0;
 
   maxspeed_ = max;
   pageindex_ = 0;
+
+  struct stat buf;
+  if (stat("/proc/vmstat", &buf) == 0
+    && buf.st_mode & S_IFREG)
+      {
+      _vmstat = true;
+      _statFileName = "/proc/vmstat";
+      }
 }
 
 PageMeter::~PageMeter( void ){
@@ -43,17 +53,69 @@ void PageMeter::checkResources( void ){
 }
 
 void PageMeter::checkevent( void ){
-  getpageinfo();
-  drawfields();
+ if (_vmstat)
+     getvmpageinfo();
+ else
+     getpageinfo();
+ drawfields();
 }
+
+void PageMeter::updateinfo(void)
+    {
+    int oldindex = (pageindex_+1)%2;
+    for ( int i = 0; i < 2; i++ )
+        {
+        if ( pageinfo_[oldindex][i] == 0 )
+            pageinfo_[oldindex][i] = pageinfo_[pageindex_][i];
+
+        fields_[i] = pageinfo_[pageindex_][i] - pageinfo_[oldindex][i];
+        total_ += fields_[i];
+        }
+
+    if ( total_ > maxspeed_ )
+        fields_[2] = 0.0;
+    else
+        {
+        fields_[2] = maxspeed_ - total_;
+        total_ = maxspeed_;
+        }
+
+    setUsed (total_ - fields_[2], maxspeed_);
+    pageindex_ = (pageindex_ + 1) % 2;
+    }
+
+void PageMeter::getvmpageinfo(void)
+    {
+    total_ = 0;
+    char buf[MAX_PROCSTAT_LENGTH];
+    std::ifstream stats(_statFileName);
+    if (!stats)
+        {
+        std::cerr <<"Cannot open file : " << _statFileName << std::endl;
+        exit(1);
+        }
+    do
+        {
+        stats >> buf;
+        } while (!stats.eof() && strncasecmp(buf, "pswpin", 7));
+    stats >>pageinfo_[pageindex_][0];
+
+    do
+        {
+        stats >> buf;
+        } while (!stats.eof() && strncasecmp(buf, "pswpout", 8));
+    stats >> pageinfo_[pageindex_][1];
+
+    updateinfo();
+    }
 
 void PageMeter::getpageinfo( void ){
   total_ = 0;
   char buf[MAX_PROCSTAT_LENGTH];
-  std::ifstream stats( STATFILENAME );
+  std::ifstream stats(_statFileName);
 
   if ( !stats ){
-    std::cerr <<"Cannot open file : " <<STATFILENAME << std::endl;
+    std::cerr <<"Cannot open file : " << _statFileName << std::endl;
     exit( 1 );
   }
 
@@ -63,23 +125,5 @@ void PageMeter::getpageinfo( void ){
 
   stats >>pageinfo_[pageindex_][0] >>pageinfo_[pageindex_][1];
 
-  int oldindex = (pageindex_+1)%2;
-
-  for ( int i = 0; i < 2; i++ ) {
-    if ( pageinfo_[oldindex][i] == 0 )
-      pageinfo_[oldindex][i] = pageinfo_[pageindex_][i];
-
-    fields_[i] = pageinfo_[pageindex_][i] - pageinfo_[oldindex][i];
-    total_ += fields_[i];
-  }
-
-  if ( total_ > maxspeed_ )
-    fields_[2] = 0.0;
-  else {
-    fields_[2] = maxspeed_ - total_;
-    total_ = maxspeed_;
-  }
-
-  setUsed (total_ - fields_[2], maxspeed_);
-  pageindex_ = (pageindex_ + 1) % 2;
+  updateinfo();
 }
--- linux/pagemeter.h
+++ linux/pagemeter.h
@@ -1,4 +1,4 @@
-//  
+//
 //  Copyright (c) 1996 by Massimiliano Ghilardi ( ghilardi@cibs.sns.it )
 //
 //  This file may be distributed under terms of the GPL
@@ -24,8 +24,12 @@ protected:
   float pageinfo_[2][2];
   int pageindex_;
   float maxspeed_;
+  bool _vmstat;
+  char *_statFileName;
 
   void getpageinfo( void );
+  void getvmpageinfo( void );
+  void updateinfo(void);
 private:
 };
 
