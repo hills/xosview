8b86098165df9044ee1dbeb7c6bbaa06c4d56ddd
romberg <romberg>
2008-02-28 23:32:48 +0000

    Apply Blars Blarson's Fix for FTBFS with sys/io.h on sparc and serialmeter.

--- linux/serialmeter.cc
+++ linux/serialmeter.cc
@@ -32,7 +32,7 @@ typedef unsigned long long u64;
 
 #include <unistd.h>
 #if defined(GNULIBC) || defined(__GLIBC__)
-#if !defined(__hppa__) && !defined(__mips__)
+#if !defined(__hppa__) && !defined(__mips__) && !defined(__sparc__)
 #include <sys/io.h>
 #endif
 #if !defined(__alpha__) && !defined(__sparc__) && !defined(__powerpc__) && !defined(__ia64__) && !defined(__hppa__) && !defined(__arm__) && !defined(__mips__)
@@ -45,7 +45,6 @@ typedef unsigned long long u64;
 #endif
 #endif
 #include <linux/serial.h>
-#include <linux/serial_reg.h>
 
 SerialMeter::SerialMeter( XOSView *parent, Device device )
   : BitMeter( parent, getTitle(device), "LSR bits(0-7), MSR bits(0-7)", 16){
