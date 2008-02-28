43de1012fd850c47d5fa9d5f55f7db2d2b5ec437
romberg <romberg>
2008-02-28 23:29:39 +0000

    Apply Fix for search path of app-default

--- Xrm.cc
+++ Xrm.cc
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 1994, 1995, 2006 by Mike Romberg ( mike.romberg@noaa.gov )
+//  Copyright (c) 1994, 1995, 2006, 2008 by Mike Romberg ( mike.romberg@noaa.gov )
 //
 //  This file may be distributed under terms of the GPL
 //
@@ -152,20 +152,29 @@ Listed from weakest to strongest:
 
   //  Merge in the system resource database.
   char rfilename[2048];
+  int result;
 
   // Get the app-defaults
-  snprintf(rfilename, 2048, "/usr/X11R6/lib/X11/app-defaults/%s",
-      XrmQuarkToString(_class));
+  result = snprintf(rfilename, sizeof rfilename, "/etc/X11/app-defaults/%s",
+    XrmQuarkToString(_class));
   if (rfilename != NULL)
+    XrmCombineFileDatabase (rfilename, &_db, 1);
+  result = snprintf(rfilename, sizeof rfilename, "/usr/lib/X11/app-defaults/%s",
+    XrmQuarkToString(_class));
+  if (result >= 0 && result < (int)sizeof(rfilename))
+    XrmCombineFileDatabase (rfilename, &_db, 1);
+  result = snprintf(rfilename, (int)sizeof rfilename, "/usr/X11R6/lib/X11/app-defaults/%s",
+    XrmQuarkToString(_class));
+  if (result >= 0 && result < (int)sizeof(rfilename))
     XrmCombineFileDatabase (rfilename, &_db, 1);
   //  Try a few more, for SunOS/Solaris folks.
-  snprintf(rfilename, 2048, "/usr/openwin/lib/X11/app-defaults/%s",
-      XrmQuarkToString(_class));
-  if (rfilename != NULL)
+  result = snprintf(rfilename, sizeof rfilename, "/usr/openwin/lib/X11/app-defaults/%s",
+    XrmQuarkToString(_class));
+  if (result >= 0 && result < (int)sizeof(rfilename))
     XrmCombineFileDatabase (rfilename, &_db, 1);
-  snprintf(rfilename, 2048, "/usr/local/X11R6/lib/X11/app-defaults/%s",
+  result = snprintf(rfilename, sizeof rfilename, "/usr/local/X11R6/lib/X11/app-defaults/%s",
       XrmQuarkToString(_class));
-  if (rfilename != NULL)
+  if (result >= 0 && result < (int)sizeof(rfilename))
     XrmCombineFileDatabase (rfilename, &_db, 1);
 
   //  Now, check for an XOSView file in the XAPPLRESDIR directory...
