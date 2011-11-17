hills/xosview

commit e0fe044dd69c1e0f10c5bad17e3cb13d8160bddb

Redraw the meter when the colours change

linux/btrymeter.cc
@@ -190,6 +190,7 @@ void BtryMeter::checkevent( void ){
 	}
 
 	drawlegend();
+	drawfields(1);
    }
 
 
@@ -232,9 +233,8 @@ void BtryMeter::checkevent( void ){
 		break;
 	}
 	drawlegend();
+	drawfields(1);
    }
-
-   drawfields();
 }
 
 

commit 6e3c54b3f2ea8b4182a94cc480dd33549311a9a5

Always redraw when device is polled
This fixes a bug introduced in commit e0fe044d.

linux/btrymeter.cc
@@ -236,13 +236,17 @@ void BtryMeter::checkevent( void ){
     handle_apm_state();
     drawlegend();
     drawfields(1);
+    return;
   }
 
   if ( old_acpi_charge_state != acpi_charge_state ) {
     handle_acpi_state();
     drawlegend();
     drawfields(1);
+    return;
   }
+
+  drawfields();
 }
 
 