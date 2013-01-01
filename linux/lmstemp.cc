8708646692048b68ddd1f0d0311f0611c0c72949
Tomi Tapper <tomi.o.tapper@student.jyu.fi>
2013-01-01 20:34:38 +0200

    Fix file finding logic in lmstemp.
    Handle correctly case where only the common part of lmstemp and lmshigh is given.
    Check that given absolute path points to a file.

--- linux/lmstemp.cc
+++ linux/lmstemp.cc
@@ -26,7 +26,7 @@ static const char PROC_SENSORS_26[] = "/sys/class/hwmon";
 
 LmsTemp::LmsTemp( XOSView *parent, const char *tempfile, const char *highfile, const char *label, const char *caption)
   : FieldMeter( parent, 3, label, caption, 1, 1, 0 ){
-  _highfile = new char[PATH_MAX];
+  _highfile = NULL;
   _tempfile = new char[PATH_MAX];
   if (!checksensors(1, PROC_SENSORS_24, tempfile, highfile)) {
     if (!checksensors(0, PROC_SENSORS_26, tempfile, highfile)) {
@@ -54,12 +54,6 @@ LmsTemp::LmsTemp( XOSView *parent, const char *tempfile, const char *highfile, c
       parent_->done(1);
     }
   }
-  _high = 0;
-  const char *p;
-  if ((p = strrchr(caption,'/')) != 0)
-    total_ = atoi(p+1);
-  else
-    total_ = 100;
 }
 
 LmsTemp::~LmsTemp( void ){
@@ -74,11 +68,13 @@ bool LmsTemp::checksensors(int isproc, const char *dir, const char* tempfile, co
   DIR *d1, *d2;
   struct dirent *ent1, *ent2;
   struct stat buf;
-  int j = (isproc ? 1 : 2);
 
+  /* First, check if absolute paths were given. */
   if (tempfile[0] == '/') {
-    if (stat(tempfile, &buf) == 0)
+    if (stat(tempfile, &buf) == 0 && S_ISREG(buf.st_mode)) {
+      strncpy(_tempfile, tempfile, PATH_MAX);
       temp_found = true;
+    }
     else
       return false;
   }
@@ -86,45 +82,44 @@ bool LmsTemp::checksensors(int isproc, const char *dir, const char* tempfile, co
     high_found = true;
   else {
     if (highfile[0] == '/') {
-      if (stat(highfile, &buf) == 0)
+      if (stat(highfile, &buf) == 0 && S_ISREG(buf.st_mode)) {
+        _highfile = new char[PATH_MAX];
+        strncpy(_highfile, highfile, PATH_MAX);
         high_found = true;
+      }
       else
         return false;
     }
   }
   if (temp_found && high_found) {
-    strcpy(_tempfile, tempfile);
-    if (highfile)
-      strcpy(_highfile, highfile);
-    else {
-      delete[] _highfile;
-      _highfile = NULL;
-    }
-    _isproc = strncmp(_tempfile, "/proc", 5) == 0 ? 1 : 0;
+    _isproc = ( strncmp(_tempfile, "/proc", 5) ? 0 : 1 );
     return true;
   }
 
+  /* Then, try to find the given file. */
   d1 = opendir(dir);
   if (!d1)
     return false;
   else {
     char dirname[64];
 
-    while (!temp_found && !high_found && (ent1 = readdir(d1))) {
-      if (!strncmp(ent1->d_name, ".", 1))
-        continue;
-      if (!strncmp(ent1->d_name, "..", 2))
+    while ( !(temp_found && high_found) && (ent1 = readdir(d1)) ) {
+      if ( !strncmp(ent1->d_name, ".", 1) ||
+           !strncmp(ent1->d_name, "..", 2) )
         continue;
 
       snprintf(dirname, 64, "%s/%s", dir, ent1->d_name);
-      for (int i = 0; i < j; i++) {
+      for (int i = 0; i < (isproc ? 1 : 2); i++) {
         if ( stat(dirname, &buf) == 0 && S_ISDIR(buf.st_mode) ) {
           d2 = opendir(dirname);
           if (!d2)
             std::cerr << "The directory " << dirname
-                      << "exists but cannot be read.\n";
+                      << "exists but cannot be read." << std::endl;
           else {
             while ((ent2 = readdir(d2))) {
+              if (!strncmp(ent2->d_name, ".", 1) ||
+                  !strncmp(ent2->d_name, "..", 2) )
+                continue;
               char f[80];
               snprintf(f, 80, "%s/%s", dirname, ent2->d_name);
               if ( stat(f, &buf) != 0 || !S_ISREG(buf.st_mode) )
@@ -134,32 +129,47 @@ bool LmsTemp::checksensors(int isproc, const char *dir, const char* tempfile, co
                 if ( !strncmp(ent2->d_name, tempfile, strlen(tempfile)) ) {
                   temp_found = true;
                   high_found = true;
-                  strcpy(_tempfile, f);
+                  strncpy(_tempfile, f, PATH_MAX);
                 }
               }
               else {
-                if ( !strncmp(ent2->d_name, tempfile, strlen(tempfile)) &&
-                     !strncmp(ent2->d_name + strlen(tempfile), "_input", 6) ) {
-                  strcpy(_tempfile, f);
-                  temp_found = true;
+                // check for tempfile and tempfile_input
+                if ( !strncmp(ent2->d_name, tempfile, strlen(tempfile)) ) {
+                  if ( !temp_found && ( strlen(tempfile) == strlen(ent2->d_name) ||
+                      !strncmp(ent2->d_name + strlen(tempfile), "_input", 6) ) ) {
+                    std::cout << f << std::endl;
+                    strncpy(_tempfile, f, PATH_MAX);
+                    temp_found = true;
+                  }
+                  // check for tempfile_max as highfile, if no highfile was given
+                  if ( !highfile && !strncmp(ent2->d_name + strlen(tempfile), "_max", 4) ) {
+                    std::cout << f << std::endl;
+                    _highfile = new char[PATH_MAX];
+                    strncpy(_highfile, f, PATH_MAX);
+                    high_found = true;
+                  }
                 }
-                if ( !high_found && (!strncmp(ent2->d_name, highfile, strlen(highfile)) &&
-                     !strncmp(ent2->d_name + strlen(highfile), "_max", 4)) ) {
-                  strcpy(_highfile, f);
-                  high_found = true;
+                // check for highfile and highfile_max
+                if ( !high_found && !strncmp(ent2->d_name, highfile, strlen(highfile)) ) {
+                  if ( strlen(highfile) == strlen(ent2->d_name) ||
+                      !strncmp(ent2->d_name + strlen(highfile), "_max", 4) ) {
+                    std::cout << f << std::endl;
+                    _highfile = new char[PATH_MAX];
+                    strncpy(_highfile, f, PATH_MAX);
+                    high_found = true;
+                  }
                 }
-              }
-              if (temp_found && high_found) {
-                _isproc = isproc;
-                closedir(d2);
-                closedir(d1);
-                return true;
               }
             }
             closedir(d2);
           }
         }
 
+        if (temp_found && high_found) {
+          _isproc = isproc;
+          closedir(d1);
+          return true;
+        }
         // Some /sys sensors have the readings in subdirectory /device
         if (!isproc)
           strncat(dirname, "/device", sizeof(dirname) - strlen(dirname) - 1);
@@ -167,7 +177,7 @@ bool LmsTemp::checksensors(int isproc, const char *dir, const char* tempfile, co
     }
   }
   closedir(d1);
-  return (temp_found && high_found);
+  return (temp_found & high_found);
 }
 
 void LmsTemp::checkResources( void ){
@@ -178,6 +188,7 @@ void LmsTemp::checkResources( void ){
   setfieldcolor( 0, _actcolor );
   setfieldcolor( 1, parent_->getResource( "lmstempIdleColor") );
   setfieldcolor( 2, _highcolor );
+  total_ = atoi( parent_->getResourceOrUseDefault( "lmstempHighest", "100" ) );
   priority_ = atoi (parent_->getResource( "lmstempPriority" ) );
   SetUsedFormat(parent_->getResource( "lmstempUsedFormat" ) );
 }
