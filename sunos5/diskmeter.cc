fe3c9e47c8e7170416ec6af50737913c0a0a4fe0
Tomi Tapper <tomi.o.tapper@student.jyu.fi>
2014-03-31 23:06:01 +0300

    Only count actual disks in Solaris diskmeter.
    Ignore partitions, USB devices, RAID and any other i/o devices.

--- sunos5/diskmeter.cc
+++ sunos5/diskmeter.cc
@@ -4,87 +4,78 @@
 //
 
 #include "diskmeter.h"
-#include "xosview.h"
-#include <fstream>
 #include <stdlib.h>
+#include <string.h>
 
 
-DiskMeter::DiskMeter( XOSView *parent, kstat_ctl_t *_kc, float max )
-  : FieldMeterGraph( parent, 3, "DISK", "READ/WRITE/IDLE")
-    {
-      kc = _kc;
-    read_prev_ = 0;
-    write_prev_ = 0;
-    maxspeed_ = max;
-    npart=0;
-    for (kstat_t *ksp = kc->kc_chain; ksp != NULL && npart <NPARTS;
-	 ksp = ksp->ks_next) {
-      if (ksp->ks_type == KSTAT_TYPE_IO ){
-	if(kstat_read(kc, ksp, NULL) != -1) {
-	  part[npart++] = ksp;
-	}
-      }
-    }
-    getdiskinfo();
-    
+DiskMeter::DiskMeter( XOSView *parent, kstat_ctl_t *kc, float max )
+    : FieldMeterGraph( parent, 3, "DISK", "READ/WRITE/IDLE" )
+{
+    _kc = kc;
+    _read_prev = _write_prev = 0;
+    _maxspeed = max;
 }
 
 DiskMeter::~DiskMeter( void )
-    {
-    }
+{
+}
 
 void DiskMeter::checkResources( void )
-    {
+{
     FieldMeterGraph::checkResources();
 
     setfieldcolor( 0, parent_->getResource("diskReadColor") );
     setfieldcolor( 1, parent_->getResource("diskWriteColor") );
     setfieldcolor( 2, parent_->getResource("diskIdleColor") );
-    priority_ = atoi (parent_->getResource( "diskPriority" ) );
-    dodecay_ = parent_->isResourceTrue("diskDecay" );
-    useGraph_ = parent_->isResourceTrue( "diskGraph" );
-    SetUsedFormat(parent_->getResource("diskUsedFormat"));
-    }
+    priority_ = atoi( parent_->getResource("diskPriority") );
+    dodecay_ = parent_->isResourceTrue("diskDecay");
+    useGraph_ = parent_->isResourceTrue("diskGraph");
+    SetUsedFormat( parent_->getResource("diskUsedFormat") );
+}
 
 void DiskMeter::checkevent( void )
-    {
+{
     getdiskinfo();
     drawfields();
-    }
+}
 
 void DiskMeter::getdiskinfo( void )
 {
-    total_ = maxspeed_;
-    kstat_io_t     kio;
-    u_longlong_t read_curr = 0;
-    u_longlong_t write_curr = 0;
+    total_ = _maxspeed;
+    kstat_io_t kio;
+    uint64_t read_curr = 0, write_curr = 0;
 
     IntervalTimerStop();
-
-    for (int i = 0; i < npart; i++) {
-      if (kstat_read(kc, part[i], &kio) == -1)
-	continue;
-      read_curr += kio.nread;
-      write_curr += kio.nwritten;
+    for (kstat_t *ksp = _kc->kc_chain; ksp != NULL; ksp = ksp->ks_next) {
+        if ( ksp->ks_type != KSTAT_TYPE_IO ||
+             strncmp(ksp->ks_class, "disk", 4) )
+            continue;
+        if ( kstat_read(_kc, ksp, &kio) == -1 )
+            continue;
+        XOSDEBUG("%s: %llu bytes read %llu bytes written.\n",
+                 ksp->ks_name, kio.nread, kio.nwritten);
+        read_curr += kio.nread;
+        write_curr += kio.nwritten;
     }
-    if(read_prev_ == 0) read_prev_ = read_curr;
-    if(write_prev_ == 0) write_prev_ = write_curr;
-    fields_[0] = (read_curr - read_prev_) / IntervalTimeInSecs();
-    fields_[1] = (write_curr - write_prev_) / IntervalTimeInSecs();
+    if (_read_prev == 0)
+        _read_prev = read_curr;
+    if (_write_prev == 0)
+        _write_prev = write_curr;
 
-    //  Adjust 
-    if (fields_[0] < 0) fields_[0] = 0.0;
-    if (fields_[1] < 0) fields_[1] = 0.0;
-    if (fields_[0] + fields_[1] > total_)
-       	total_ = fields_[0] + fields_[1];
+    double t = IntervalTimeInSecs();
+    fields_[0] = (double)(read_curr - _read_prev) / t;
+    fields_[1] = (double)(write_curr - _write_prev) / t;
 
-    fields_[2] = total_ - (fields_[0] + fields_[1]);
-    
-    read_prev_ = read_curr;
-    write_prev_ = write_curr;
-    
-    //setUsed((fields_[0]+fields_[1]) * IntervalTimeInMicrosecs()/1e6, total_);
-    // give rate in units per second, not units per interval
-    setUsed((fields_[0]+fields_[1]), total_);
     IntervalTimerStart();
+    _read_prev = read_curr;
+    _write_prev = write_curr;
+
+    if (fields_[0] < 0)
+        fields_[0] = 0;
+    if (fields_[1] < 0)
+        fields_[1] = 0;
+    if (fields_[0] + fields_[1] > total_)
+        total_ = fields_[0] + fields_[1];
+    fields_[2] = total_ - (fields_[0] + fields_[1]);
+    setUsed(fields_[0] + fields_[1], total_);
 }
--- sunos5/diskmeter.h
+++ sunos5/diskmeter.h
@@ -8,29 +8,26 @@
 #define _DISKMETER_H_
 
 #include "fieldmetergraph.h"
+#include "xosview.h"
 #include <kstat.h>
-#define NPARTS 100
 
-class DiskMeter : public FieldMeterGraph 
-    {
-    public:
-        DiskMeter( XOSView *parent, kstat_ctl_t *_kc, float max );
-        ~DiskMeter( void );
 
-        const char *name( void ) const { return "DiskMeter"; }
-        void checkevent( void );
-        
-        void checkResources( void );
-    protected:
+class DiskMeter : public FieldMeterGraph {
+ public:
+    DiskMeter( XOSView *parent, kstat_ctl_t *kc, float max );
+    ~DiskMeter( void );
 
-        void getdiskinfo( void );
-    private:
-        u_longlong_t read_prev_;
-        u_longlong_t write_prev_;
-        float maxspeed_;
-	int npart;
-	kstat_ctl_t *kc;
-	kstat_t *part[NPARTS];
-    };
+    const char *name( void ) const { return "DiskMeter"; }
+    void checkevent( void );
+    void checkResources( void );
+
+ protected:
+    void getdiskinfo( void );
+
+ private:
+    uint64_t _read_prev, _write_prev;
+    float _maxspeed;
+    kstat_ctl_t *_kc;
+};
 
 #endif
