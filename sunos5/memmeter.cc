e0c076e3a454020b17ba9f152e044bb6894c61a6
Tomi Tapper <tomi.o.tapper@student.jyu.fi>
2014-04-02 10:25:37 +0300

    Add fields for kernel memory and ZFS file data to Solaris memmeter.

--- sunos5/memmeter.cc
+++ sunos5/memmeter.cc
@@ -2,20 +2,22 @@
 //  Initial port performed by Greg Onufer (exodus@cheers.bungi.com)
 //
 #include "memmeter.h"
-#include "xosview.h"
 #include <unistd.h>
 #include <stdlib.h>
+#include <iostream>
+
 
 MemMeter::MemMeter(XOSView *parent, kstat_ctl_t *_kc)
-	: FieldMeterGraph(parent, 2, "MEM", "USED/FREE")
+	: FieldMeterGraph(parent, 4, "MEM", "SYS/ZFS/OTHER/FREE")
 {
 	kc = _kc;
-
-	_pageSize = sysconf(_SC_PAGESIZE);
+	pageSize = sysconf(_SC_PAGESIZE);
 	total_ = sysconf(_SC_PHYS_PAGES);
 
-	ksp = kstat_lookup(kc, "unix", 0, "system_pages");
-	if (ksp == NULL) {
+	ksp_sp = kstat_lookup(kc, "unix", 0, "system_pages");
+	ksp_zfs = kstat_lookup(kc, "vmem", -1, "zfs_file_data_buf");
+	if (ksp_sp == NULL) { // ZFS cache may be missing
+		std::cerr << "Can not find unix:0:system_pages kstat." << std::endl;
 		parent_->done(1);
 		return;
 	}
@@ -25,9 +27,11 @@ void MemMeter::checkResources(void)
 {
 	FieldMeterGraph::checkResources();
 
-	setfieldcolor(0, parent_->getResource("memUsedColor"));
-	setfieldcolor(1, parent_->getResource("memFreeColor"));
-	priority_ = atoi (parent_->getResource("memPriority"));
+	setfieldcolor(0, parent_->getResource("memKernelColor"));
+	setfieldcolor(1, parent_->getResource("memCacheColor"));
+	setfieldcolor(2, parent_->getResource("memUsedColor"));
+	setfieldcolor(3, parent_->getResource("memFreeColor"));
+	priority_ = atoi(parent_->getResource("memPriority"));
 	dodecay_ = parent_->isResourceTrue("memDecay");
 	useGraph_ = parent_->isResourceTrue("memGraph");
 	SetUsedFormat(parent_->getResource("memUsedFormat"));
@@ -47,17 +51,49 @@ void MemMeter::getmeminfo(void)
 {
 	kstat_named_t *k;
 
-	if (kstat_read(kc, ksp, NULL) == -1) {
-		parent_->done(1);
-		return;
+	fields_[1] = 0;
+	if (ksp_zfs) {
+		if (kstat_read(kc, ksp_zfs, NULL) == -1) {
+			std::cerr << "Can not read vmem::zfs_file_data_buf kstat." << std::endl;
+			parent_->done(1);
+			return;
+		}
+		k = (kstat_named_t *)kstat_data_lookup(ksp_zfs, "mem_inuse");
+		if (k == NULL) {
+			std::cerr << "Can not read vmem::zfs_file_data_buf:mem_inuse kstat." << std::endl;
+			parent_->done(1);
+			return;
+		}
+		fields_[1] = k->value.ui64 / pageSize;
 	}
-	k = (kstat_named_t *)kstat_data_lookup(ksp, "freemem");
-	if (k == NULL) {
-		parent_->done(1);
-		return;
-	}
-	fields_[0] = total_ - k->value.l;
-	fields_[1] = k->value.l;
 
-	FieldMeterDecay::setUsed(fields_[0] * _pageSize, total_ * _pageSize);
+	if (kstat_read(kc, ksp_sp, NULL) == -1) {
+		std::cerr << "Can not read unix:0:system_pages kstat." << std::endl;
+		parent_->done(1);
+		return;
+	}
+	k = (kstat_named_t *)kstat_data_lookup(ksp_sp, "pp_kernel");
+	if (k == NULL) {
+		std::cerr << "Can not read unix:0:system_pages:pp_kernel kstat." << std::endl;
+		parent_->done(1);
+		return;
+	}
+	fields_[0] = k->value.l - fields_[1];
+	k = (kstat_named_t *)kstat_data_lookup(ksp_sp, "freemem");
+	if (k == NULL) {
+		std::cerr << "Can not read unix:0:system_pages:freemem kstat." << std::endl;
+		parent_->done(1);
+		return;
+	}
+	fields_[3] = k->value.l;
+	fields_[2] = total_ - (fields_[0] + fields_[1] + fields_[3]);
+
+	XOSDEBUG("kernel: %lld kB zfs: %lld kB other: %lld kB free: %lld kB\n",
+	         (uint64_t)fields_[0] * pageSize / 1024,
+	         (uint64_t)fields_[1] * pageSize / 1024,
+	         (uint64_t)fields_[2] * pageSize / 1024,
+	         (uint64_t)fields_[3] * pageSize / 1024);
+
+	setUsed((fields_[0] + fields_[1] + fields_[2]) * pageSize, total_ * pageSize);
 }
+
--- sunos5/memmeter.h
+++ sunos5/memmeter.h
@@ -5,27 +5,26 @@
 #define _MEMMETER_H_
 
 #include "fieldmetergraph.h"
+#include "xosview.h"
 #include <kstat.h>
+
 
 class MemMeter : public FieldMeterGraph {
  public:
 	MemMeter(XOSView *parent, kstat_ctl_t *kcp);
 	~MemMeter(void);
 
-	const char *name(void) const { return "MemMeter"; }  
+	const char *name(void) const { return "MemMeter"; }
 	void checkevent( void );
-
 	void checkResources(void);
 
  protected:
-	//  struct pst_status *stats_;
-	int _pageSize;
-
 	void getmeminfo( void );
 
  private:
+	int pageSize;
 	kstat_ctl_t *kc;
-	kstat_t *ksp;
+	kstat_t *ksp_sp, *ksp_zfs;
 };
 
 
