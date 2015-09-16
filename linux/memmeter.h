607d58bf3724bdacfecd9d7ff78f7f1838c0e08f
Mark Hills <mark.hills@framestore.com>
2015-09-16 14:30:12 +0100

    Replace the parsing of /proc/meminfo
    We have seen multiple problems with the existing code which manifest
    themselves as either sporadic or consistent displays of warnings such
    as:
    
      meter MemMeter had a negative value of xxxxxxxxxxxxxxxxx
    
    The previous code was hard to read due to multiple levels of
    indirection; firstly it puts content into struct LineInfo, then maps
    these to field_[] values. It's possible the fields are used incorrectly,
    and we wanted to audit the comment 'mapped comes from cached'.
    
    Improve the code by:
    
    * Removing the levels of indirection through the arrays of fields
      and using named variables instead
    
    * Remove the intermediate calculations which are done using double;
      it's likely the approximation caused by floating point is the cause
      of the sporadic errors when memory is tight and some values overflow.
    
    * Be consistent to the comment 'cached comes from mapped'; if this is
      true then 'mapped' should be excluded from the calculation of fields_[0].
    
    * Use a simple parser using stdio; the code is already using the
      C library and it allows us to report errors on opening the file
      and my experiences in the other parts of the code showed it to be
      less hungry on resources than fstream (this code is used in a tight
      loop)
    
    The result is a significant decrease in lines of code, increase in
    readability and hopefully fixes the above bugs (or at least possible
    bugs!)

--- linux/memmeter.h
+++ linux/memmeter.h
@@ -15,42 +15,13 @@
 class MemMeter : public FieldMeterGraph {
 public:
   MemMeter( XOSView *parent );
-  ~MemMeter( void );
 
   const char *name( void ) const { return "MemMeter"; }
   void checkevent( void );
-
   void checkResources( void );
-protected:
 
-  void getmeminfo( void );
 private:
-  class LineInfo {
-  public:
-    LineInfo(const char *id, double *val)
-      { _line = -1; _id = id; _val = val; _idlen = strlen(_id); }
-    LineInfo(void) {};
-
-    int line(void) { return _line; }
-    void line(int l) { _line = l; }
-    const char *id(void) { return _id; }
-    int idlen(void) { return _idlen; }
-
-    void setVal(double val) { *_val = val; }
-
-  private:
-    int _line;
-    const char *_id;
-    int _idlen;
-    double *_val;
-  };
-
-  LineInfo *_MIlineInfos;
-  int _numMIlineInfos;
-
-  void initLineInfo(void);
-  LineInfo *findLines(LineInfo *tmplate, int len, const char *fname);
-  void getmemstat(const char *fname, LineInfo *infos, int ninfos);
+  void getstats( void );
 };
 
 
