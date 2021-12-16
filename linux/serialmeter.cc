hills/xosview

commit b48dd68ece0cd6c4d24f1b629ed9500bc86a554e

Invert this match for our sanity
Maintainers keep having to add exceptions here. In reality the check
for the specific case is better.

I inferred which platforms were supported by looking at glibc source,
and then specifically removed Alpha because this was explicitly listed
in the previous code as not supported. I don't have access to any
platforms other than amd64 for testing.

linux/serialmeter.cc
@@ -18,8 +18,11 @@
 #include <sstream>
 #include <iomanip>
 
-
-#if !defined(__alpha__) && !defined(__sparc__) && !defined(__powerpc__) && !defined(__ia64__) && !defined(__hppa__) && !defined(__arm__) && !defined(__mips__) && !defined(__sh__) && !defined(__s390__) && !defined (__s390x__) && !defined(__m68k__) && !defined(__aarch64__)
+/*
+ * To fetch status information requires ioperm() and inb()
+ * otherwise these meter is largely a no-op.
+ */
+#if defined(__i386__) || defined(__ia64__) || defined(__amd64__)
 #include <sys/io.h>
 #include <sys/perm.h>
 #define HAVE_IOPERM

commit af4aec777645516a1fc06995bba1591d7fdc594b

Fix build regression on ia64
In b48dd68, the exclusion list of architectures for ioperm() was
inverted resulting in ia64 accidentally being added as a supported
architecture. However, the ioperm() is not available on ia64 which
causes xosview failing to build on this architecture, thus remove
ia64 from the list of supported architectures.

linux/serialmeter.cc
@@ -23,7 +23,7 @@
  * To fetch status information requires ioperm() and inb()
  * otherwise these meter is largely a no-op.
  */
-#if defined(__i386__) || defined(__ia64__) || defined(__amd64__)
+#if defined(__i386__) || defined(__amd64__)
 #include <sys/io.h>
 #include <sys/perm.h>
 #define HAVE_IOPERM